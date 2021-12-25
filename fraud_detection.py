949b4d97abfdc63df8610cede3c706cba7309e09
ruhal <ruhalmiah@gmail.com>
2021-12-25 20:36:51 +0000

    added sample

--- fraud_detection.py
+++ fraud_detection.py
@@ -24,6 +24,7 @@ credit_card['Class'].value_counts()
 credit_card['Class'].value_counts()/credit_card['Class'].value_counts().sum()
 
 # Pie chart of fraud and legit transactions
+plt.figure(dpi=300)
 plt.pie(credit_card['Class'].value_counts()/credit_card['Class'].value_counts().sum(),
         labels = ["Legit", "Fraud"], colors = ['b','r'], autopct='%.2f')
 plt.title("Pie chart of credit card transactions")
@@ -31,8 +32,20 @@ plt.show()
 
 #------------------------------------------------------------------------
 
-# Prediction with no model
+# Prediction with no model, assuming all transactions are legit
 predictions = np.zeros((credit_card.shape[0],), dtype=int)
 predictions = pd.Categorical(predictions, categories=[0, 1])
-sklm.confusion_matrix(credit_card['Class'], predictions)
-sklm.confusion_matrix()
+conf_mat = sklm.confusion_matrix(credit_card['Class'], predictions)
+print(conf_mat)
+print(sklm.classification_report(credit_card['Class'], predictions, zero_division=False, digits=4))
+
+#------------------------------------------------------------------------
+
+# Random sample with replacement with fixed seed
+cc_sample = credit_card.sample(frac=0.1, replace=True, random_state=1)
+
+cc_sample['Class'].value_counts()
+
+plt.figure(dpi=300)
+plt.scatter(cc_sample['V1'], cc_sample['V2'], c=cc_sample['Class'], s=3)
+plt.show()
